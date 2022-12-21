40d374d47a858c93af0dbed87f5f9b37a09c4501
Tamara Brkic <brkictamara7@gmail.com>
2022-12-21 20:11:47 +0100

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa';
+
+function Home() {
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          Have you recently invested in crypto? Or you just track your
+          investment history?
+        </h1>
+        <p>Please choose from an option below</p>
+      </section>
+    </>
+  );
+}
+
+export default Home;
